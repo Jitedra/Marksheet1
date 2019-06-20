6ad8926df592e54cbc733958fe5a1581bebd535a
Jitendra <jitendra12gupta@gmail.com>
2019-06-21 01:43:50 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -7,7 +7,36 @@ function check(){
   let science = document.getElementById('science').value;
   let hindi = document.getElementById('hindi').value;
 
-  let total = parseInt(math) + parseInt(english) + parseInt(social) + parseInt(science) + parseInt(hindi);
+  console.log(hindi);
 
-  alert('Total marks obtained ' + total);
+  const stu1 = {
+  	name: name,
+  	class: cls,
+  	math: math,
+  	english: english,
+  	social: social,
+  	science: science,
+  	hindi: hindi
+  	}; 
+    
+    window.localStorage.setItem('student', JSON.stringify(stu1));
+}
+
+function fetch(){
+  let get = JSON.parse(window.localStorage.getItem('student'));
+  // console.log(get);
+  var a = document.getElementById('name1');
+  a.value = get.name;
+  var b = document.getElementById('class1');
+  b.value = get.class;
+  var c = document.getElementById('math1');
+  c.value = get.math;
+  var d = document.getElementById('english1');
+  d.value = get.english;
+  var e = document.getElementById('social1');
+  e.value = get.social;
+  var f = document.getElementById('science1');
+  f.value = get.science;
+  var g = document.getElementById('hindi1');
+  g.value = get.hindi;
 }
