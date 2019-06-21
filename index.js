33ffff3660307e3998cabc2a96c8f5618f781337
Jitendra <jitendra12gupta@gmail.com>
2019-06-21 16:18:56 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function check(){
+  
   let name = document.getElementById('name').value;
   let cls = document.getElementById('class').value;
   let math = document.getElementById('math').value;
@@ -7,38 +8,69 @@ function check(){
   let science = document.getElementById('science').value;
   let hindi = document.getElementById('hindi').value;
 
-  console.log(hindi);
+  let stu1 = {
+    name: "",
+    class: "",
+    math: "",
+    english: "",
+    social: "",
+    science: "",
+    hindi: ""
+    };
 
-  const stu1 = {
-  	name: name,
-  	class: cls,
-  	math: math,
-  	english: english,
-  	social: social,
-  	science: science,
-  	hindi: hindi
-  	}; 
+  stu1.name = name;
+  stu1.class = cls;
+  stu1.math = math;
+  stu1.english = english;
+  stu1.social = social;
+  stu1.science = science;
+  stu1.hindi = hindi;
+
+  let fetch = JSON.parse(localStorage.getItem('student'));
+
+  let array = [];
+
+  if(fetch != null){
+    fetch.map(function (v){
+      array.push(v);
+    });
+  }
+
+
+  array.push(stu1);
     
-    window.localStorage.setItem('student', JSON.stringify(stu1));
+  window.localStorage.setItem('student', JSON.stringify(array));
 
-    window.location.href = 'thank.html';
+  window.location.href = 'thank.html';
+    
 }
 
 function fetch(){
+
+  let stu = document.getElementById('name').value;
+
+  let cls = document.getElementById('class').value;
+
   let get = JSON.parse(window.localStorage.getItem('student'));
-  // console.log(get);
-  var a = document.getElementById('name1');
-  a.value = get.name;
-  var b = document.getElementById('class1');
-  b.value = get.class;
-  var c = document.getElementById('math1');
-  c.value = get.math;
-  var d = document.getElementById('english1');
-  d.value = get.english;
-  var e = document.getElementById('social1');
-  e.value = get.social;
-  var f = document.getElementById('science1');
-  f.value = get.science;
-  var g = document.getElementById('hindi1');
-  g.value = get.hindi;
+  
+  get.map(function(v){
+    if(v.name === stu && v.class === cls){
+
+      var a = document.getElementById('name1');
+      a.value = v.name;
+      var b = document.getElementById('class1');
+      b.value = v.class;
+      var c = document.getElementById('math1');
+      c.value = v.math;
+      var d = document.getElementById('english1');
+      d.value = v.english;
+      var e = document.getElementById('social1');
+      e.value = v.social;
+      var f = document.getElementById('science1');
+      f.value = v.science;
+      var g = document.getElementById('hindi1');
+      g.value = v.hindi;
+    }
+    
+  });
 }
