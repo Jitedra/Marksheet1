2690e2d08ea295c9cb0b00bfb345d22b09334964
Jitendra <jitendra12gupta@gmail.com>
2019-06-21 18:40:38 +0530

    Edited all the commits.
    1. var to let
    2. fixed variables
    3. Indentation
    4. proper message to commit

--- index.js
+++ index.js
@@ -1,76 +1,54 @@
 function check(){
+  const name = document.getElementById('name').value;
+  const standard = document.getElementById('class').value;
+  const math = document.getElementById('math').value;
+  const english = document.getElementById('english').value;
+  const social = document.getElementById('social').value;
+  const science = document.getElementById('science').value;
+  const hindi = document.getElementById('hindi').value;
   
-  let name = document.getElementById('name').value;
-  let cls = document.getElementById('class').value;
-  let math = document.getElementById('math').value;
-  let english = document.getElementById('english').value;
-  let social = document.getElementById('social').value;
-  let science = document.getElementById('science').value;
-  let hindi = document.getElementById('hindi').value;
-
-  let stu1 = {
-    name: "",
-    class: "",
-    math: "",
-    english: "",
-    social: "",
-    science: "",
-    hindi: ""
-    };
-
-  stu1.name = name;
-  stu1.class = cls;
-  stu1.math = math;
-  stu1.english = english;
-  stu1.social = social;
-  stu1.science = science;
-  stu1.hindi = hindi;
+  let student = {
+    name: "name",
+    class: "standard",
+    math: "math",
+    english: "english",
+    social: "social",
+    science: "science",
+    hindi: "hindi"
+  };
 
   let fetch = JSON.parse(localStorage.getItem('student'));
-
   let array = [];
-
   if(fetch != null){
     fetch.map(function (v){
       array.push(v);
     });
   }
-
-
-  array.push(stu1);
-    
+  array.push(student); 
   window.localStorage.setItem('student', JSON.stringify(array));
-
   window.location.href = 'thank.html';
-    
 }
 
 function fetch(){
-
-  let stu = document.getElementById('name').value;
-
-  let cls = document.getElementById('class').value;
-
+  let student = document.getElementById('studentName').value;
+  let standard = document.getElementById('studentClass').value;
   let get = JSON.parse(window.localStorage.getItem('student'));
-  
   get.map(function(v){
-    if(v.name === stu && v.class === cls){
-
-      var a = document.getElementById('name1');
+    if(v.name === student && v.class === standard){
+      let a = document.getElementById('name');
       a.value = v.name;
-      var b = document.getElementById('class1');
+      let b = document.getElementById('class');
       b.value = v.class;
-      var c = document.getElementById('math1');
+      let c = document.getElementById('math');
       c.value = v.math;
-      var d = document.getElementById('english1');
+      let d = document.getElementById('english');
       d.value = v.english;
-      var e = document.getElementById('social1');
+      let e = document.getElementById('social');
       e.value = v.social;
-      var f = document.getElementById('science1');
+      let f = document.getElementById('science');
       f.value = v.science;
-      var g = document.getElementById('hindi1');
+      let g = document.getElementById('hindi');
       g.value = v.hindi;
     }
-    
   });
 }
